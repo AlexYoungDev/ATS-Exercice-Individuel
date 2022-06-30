5d28d70cb76f11f61ca6cb3d29c2498148792fd2
LAPTOT-AL-U4EG7\alexl <alexluu13@gmail.com>
2022-06-30 16:00:57 +0200

    new

--- html_template/js/script.js
+++ html_template/js/script.js
@@ -1,1 +1,8 @@
+<button onclick="myFunction()">Try it</button>
 
+function myFunction () {
+let firstName = prompt("Quel est ton prénom ?");
+let hello = "👋Bonjour" + firstName + "!";
+document.body.innerHTML += '<h2> hello </h2>’
+console.log (hello)
+}
